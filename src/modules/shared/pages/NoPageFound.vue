52962b883d65681286ab8c5602f8f4eed9f3edd2
Jandronit <jandrohen@gmail.com>
2022-10-29 19:06:14 +0200

    (lesson 120 course vue) Add No page found

---
+++ src/modules/shared/pages/NoPageFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="no-page-found">
+    <h1>404</h1>
+    <h2>Page not found</h2>
+    <p>The page you are looking for does not exist.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NoPageFound'
+}
+</script>
+
+<style>
+.no-page-found {
+  text-align: center;
+  margin-top: 100px;
+  h1 {
+    font-size: 100px;
+    font-weight: 100;
+  }
+  h2 {
+    font-size: 30px;
+    font-weight: 100;
+  }
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,10 +4,14 @@ import ListPage from '@/modules/pokemon/pages/ListPage';
 import AboutPage from '@/modules/pokemon/pages/AboutPage';
 import PokemonPage from '@/modules/pokemon/pages/PokemonPage';
 
+import NoPageFound from '@/modules/shared/pages/NoPageFound';
+
+
 const routes = [
     { path: '/', component: ListPage },
     { path: '/about', component: AboutPage },
     { path: '/id', component: PokemonPage },
+    { path: '/:pathMatch(.*)*', component: NoPageFound }
 ]
 
 const router = createRouter({
