02756456bd565b698f2ba485ecdbab5873b78df5
Jandronit <jandrohen@gmail.com>
2022-10-31 22:59:49 +0100

    (lesson 125 course vue) trigger lifecycle methods

--- src/modules/pokemon/pages/AboutPage.vue
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -1,10 +1,72 @@
 <template>
   <h1>About Page</h1>
+  <hr>
+  <h2>{{ message }}</h2>
+  <button @click="onChangeName">
+    Cambiar mensaje
+  </button>
 </template>
 
 <script>
 export default {
-  name: "AboutPage"
+  data() {
+    return {
+      message: 'Hello World'
+    }
+  },
+  methods: {
+    onChangeName() {
+      console.log('cambiando mensaje')
+      this.message = 'Hello Vue'
+    }
+  },
+  name: "AboutPage",
+  beforeCreate() {
+    console.log("AboutPage beforeCreate");
+  },
+  created() {
+    console.log("AboutPage created");
+  },
+  beforeMount() {
+    console.log("AboutPage beforeMount");
+  },
+  mounted() {
+    console.log("AboutPage mounted");
+  },
+  beforeUpdate() {
+    console.log("AboutPage beforeUpdate");
+  },
+  updated() {
+    console.log("AboutPage updated");
+  },
+  activated() {
+    console.log("AboutPage activated");
+  },
+  deactivated() {
+    console.log("AboutPage deactivated");
+  },
+  beforeUnmount() {
+    console.log("AboutPage beforeUnmount");
+  },
+  unmounted() {
+    console.log("AboutPage unmounted");
+  },
+  errorCaptured() {
+    console.log("AboutPage errorCaptured");
+  },
+  renderTracked() {
+    console.log("AboutPage renderTracked");
+  },
+  renderTriggered() {
+    console.log("AboutPage renderTriggered");
+  },
+  beforeDestroy() {
+    console.log("AboutPage beforeDestroy");
+  },
+  destroyed() {
+    console.log("AboutPage destroyed");
+  },
+
 }
 </script>
 
