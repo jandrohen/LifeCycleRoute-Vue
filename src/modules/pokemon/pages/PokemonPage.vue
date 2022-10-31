9500fee8d0e4fe7e14e3cde7a24f798515d89891
Jandronit <jandrohen@gmail.com>
2022-10-31 23:40:21 +0100

    (lesson 128 course vue) HTTPS requests and redirect

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,5 +1,8 @@
 <template>
   <h1>Pokemon: <span>#{{ id }}</span></h1>
+  <div v-if="pokemon">
+    <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+  </div>
 </template>
 
 <script>
@@ -11,17 +14,40 @@ export default {
       required: true
     }
   },
+
   name: "PokemonPage",
 
   data() {
     return {
       // id: null
+      pokemon: null
     };
   },
+
   created() {
     // const { id } = this.$route.params;
     // this.id = id;
     // console.log(id);
+    this.fetchPokemon();
+  },
+
+  methods: {
+    async fetchPokemon() {
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${ this.id }`)
+        this.pokemon = await response.json();
+        console.log(this.pokemon);
+      } catch (error) {
+        this.$router.push('/');
+        console.log('No hay nada que hacer')
+      }
+    }
+  },
+
+  watch: {
+    id() {
+      this.fetchPokemon();
+    }
   }
 
 }
