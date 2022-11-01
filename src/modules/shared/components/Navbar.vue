0e557553e53a17a873a9d79acb2d48e51774e58b
Jandronit <jandrohen@gmail.com>
2022-11-01 10:52:15 +0100

    (lesson 129 course vue) Redirect from the router

--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="navbar">
-    <router-link to="/">Pokemon List</router-link>
+    <router-link :to="{ name: 'home' }">Pokemon List</router-link>
     <router-link :to="{ name: 'pokemon-id', params: { id: 32 } }">Pokemon por id</router-link>
-    <router-link to="/about">About</router-link>
+    <router-link :to="{ name: 'about' }">About</router-link>
   </div>
 </template>
 
