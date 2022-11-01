3080d7931e01efd04609acae02f5fd9827adc35d
Jandronit <jandrohen@gmail.com>
2022-11-01 11:22:34 +0100

    (lesson 129 course vue) Custom RouterLink

---
+++ src/modules/shared/components/CustomLink.vue
@@ -0,0 +1,45 @@
+<template>
+  <a v-if="isExternalLink"
+     target="_blank"
+     :href="link.to"
+     class="normal-link" >{{ link.name }}</a>
+
+  <router-link
+      v-else
+      :to="link.to"
+      v-slot="{ href, isActive }">
+    <a :href="href"
+       :class=" isActive ? 'is-active' : 'normal-link'"> {{ link.name }} </a>
+  </router-link>
+
+</template>
+
+<script>
+export default {
+  name: "CustomLink",
+
+  props: {
+    link: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    isExternalLink() {
+      return this.link.to.startsWith('http');
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.is-active {
+  color: #42b983;
+}
+.normal-link {
+  color: #2c3e50;
+  font-weight: bold;
+  margin: 0 0.5rem;
+}
+</style>
--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="navbar">
-    <router-link :to="{ name: 'home' }">Pokemon List</router-link>
-    <router-link :to="{ name: 'pokemon-id', params: { id: 32 } }">Pokemon por id</router-link>
-    <router-link :to="{ name: 'about' }">About</router-link>
+<!--    <router-link :to="{ name: 'home' }">Pokemon List</router-link>-->
+<!--    <router-link :to="{ name: 'pokemon-id', params: { id: 32 } }">Pokemon por id</router-link>-->
+<!--    <router-link :to="{ name: 'about' }">About</router-link>-->
+    <CustomLink
+        v-for="link in links"
+        :key="link.to"
+        :link="link"
+    />
   </div>
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue';
+
 export default {
-  name: "Navbar"
+  name: "Navbar",
+
+  data() {
+    return {
+      links:[
+        { to: '/home', name: 'Pokemons'},
+        { to: '/pokemonid/50', name: 'Por ID'},
+        { to: '/about', name: 'About'},
+        { to: 'https://google.com', name: 'Google'}
+      ]
+    }
+  },
+
+  components: {
+   CustomLink: defineAsyncComponent(() => import('./CustomLink.vue'))
+  }
+
 }
 </script>
 
